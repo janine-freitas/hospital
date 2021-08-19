3e6520b0ecd1a6f2afb96cb0491979b9a19e57bb
janine.freitas@nubank.com.br <janine.freitas@nubank.com.br>
2021-08-19 15:24:19 -0300

    filas ao inves de vetores

--- src/hospital/logic.clj
+++ src/hospital/logic.clj
@@ -32,8 +32,33 @@
   (if (cabe-na-fila? hospital departamento)
     (update hospital departamento conj pessoa)))
 
+;(defn chega-em
+;     [hospital departamento pessoa]
+;     (if-let [novo-hospital (tenta-colocar-na-fila hospital departamento pessoa)]
+;       {:hospital novo-hospital, :resultado :sucesso}
+;       {:hospital hospital, :resultado :impossivel-colocar-pessoa-na-fila}))
+
 (defn chega-em
-     [hospital departamento pessoa]
-     (if-let [novo-hospital (tenta-colocar-na-fila hospital departamento pessoa)]
-       {:hospital novo-hospital, :resultado :sucesso}
-       {:hospital hospital, :resultado :impossivel-colocar-pessoa-na-fila}))
+  [hospital departamento pessoa]
+  (if (cabe-na-fila? hospital departamento)
+    (update hospital departamento conj pessoa)
+    (throw (ex-info "Não cabe ninguem neste departamento" {:paciente pessoa}))))
+
+(defn atende
+  [hospital departamento]
+  (update hospital departamento pop))
+
+(defn proxima
+  "Retorna o proximo da fila"
+  [hospital departamento]
+  (-> hospital
+      departamento
+      peek))
+
+(defn transfere
+  "Tranfere para o proximo paciente da fila_de para a fila_para"
+  [hospital de para]
+  (let [pessoa (proxima hospital de)]
+    (-> hospital
+        (atende de)
+        (chega-em para pessoa))))
--- test/hospital/logic_test.clj
+++ test/hospital/logic_test.clj
@@ -1,36 +1,60 @@
 (ns hospital.logic-test
+  (:use clojure.pprint)
   (:require [clojure.test :refer :all]
-            [hospital.logic :refer :all]))
+            [hospital.logic :refer :all]
+            [hospital.model :as h.model]))
 
 (deftest cabe-na-fila?-test
-  (testing "Que cabe na fila"
-    (is (cabe-na-fila? {:espera []}, :espera)))
+  (let [hospital-cheio {:espera [1 2 3 4 5]}]
+    (testing "Que cabe na fila"
+      (is (cabe-na-fila? {:espera []}, :espera)))
 
-  (testing "Que nao cabe na fila quando a fila está cheia"
-    (is (not (cabe-na-fila? {:espera [1 2 3 4 5]}, :espera))))
+    (testing "Que nao cabe na fila quando a fila está cheia"
+      (is (not (cabe-na-fila? hospital-cheio, :espera))))
 
-  (testing "Que nao cabe na fila quando tem mais do que cinco"
-    (is (not (cabe-na-fila? {:espera [1 2 3 4 5 6]}, :espera))))
+    (testing "Que nao cabe na fila quando tem mais do que cinco"
+      (is (not (cabe-na-fila? {:espera [1 2 3 4 5 6]}, :espera))))
 
-  (testing "Que cabe na fila quando tem menos do que cinco"
-    (is (cabe-na-fila? {:espera [1 2 3 4]}, :espera))
-    (is (cabe-na-fila? {:espera [1 2]}, :espera)))
+    (testing "Que cabe na fila quando tem menos do que cinco"
+      (is (cabe-na-fila? {:espera [1 2 3 4]}, :espera))
+      (is (cabe-na-fila? {:espera [1 2]}, :espera)))
 
-  (testing "Caso em que cabe na fila mas o departamento não existe"
-         (is (not (cabe-na-fila? {:espera [1 2 3 4]}, :raio-x)))))
+    (testing "Caso em que cabe na fila mas o departamento não existe"
+      (is (not (cabe-na-fila? {:espera [1 2 3 4]}, :raio-x))))))
 
 (deftest chega-em-test
-  (testing "Aceita quando cabe na fila"
-    (is (= {:hospital {:espera [1 2 3 4 5]}, :resultado :sucesso}
-           (chega-em {:espera [1 2 3 4]}, :espera 5 )))
-    (is (= {:hospital {:espera [1 2 5]}, :resultado :sucesso}
-           (chega-em {:espera [1 2]}, :espera 5 ))))
-  (testing "Nao aceita quando nao cabe na fila"
-    (is (= {:hospital {:espera [1 35 42 64 21]}, :resultado :impossivel-colocar-pessoa-na-fila}
-        (chega-em {:espera [1 35 42 64 21]}, :espera 76 ))))
-  )
+  (let [hospital-cheio {:espera [1 35 42 64 21]}]
+    ;(testing "Aceita quando cabe na fila"
+    ;  (is (= {:hospital {:espera [1 2 3 4 5]}, :resultado :sucesso}
+    ;         (chega-em {:espera [1 2 3 4]}, :espera 5 )))
+    ;  (is (= {:hospital {:espera [1 2 5]}, :resultado :sucesso}
+    ;         (chega-em {:espera [1 2]}, :espera 5 ))))
+    ;(testing "Nao aceita quando nao cabe na fila"
+    ;  (is (= {:hospital hospital-ceio, :resultado :impossivel-colocar-pessoa-na-fila}
+    ;         (chega-em hospital-ceio, :espera 76 ))))))
+    (testing "Aceita quando cabe na fila"
+      (is (= {:espera [1 2 3 4 5]}
+             (chega-em {:espera [1 2 3 4]}, :espera, 5)))
+      (is (= {:espera [1 2 5]}
+             (chega-em {:espera [1 2]}, :espera, 5))))
 
+    (testing "Nao aceita quando nao cabe na fila"
+      (is (thrown? clojure.lang.ExceptionInfo
+           (chega-em hospital-cheio, :espera 76))))))
 
-
-
-
+(deftest transfere-test
+  (testing "aceita pessoas se couber"
+    (let [hospital-original {:espera [5], :raio-x []}]
+      (is (= {:espera [] :raio-x [5]}
+             (transfere hospital-original :espera :raio-x))))
+    )
+    (let [hospital-original {:espera (conj h.model/fila-vazia 51 5), :raio-x (conj h.model/fila-vazia 13)}]
+      (pprint (transfere hospital-original :espera :raio-x))
+      (is (= {:espera [5] :raio-x [13 51]}
+             (transfere hospital-original :espera :raio-x))))
+    (testing "recusa pessoas se não couber"
+      (let [hospital-cheio {:espera [5], :raio-x [1 2 53 42 13]}]
+        (is (thrown? clojure.lang.ExceptionInfo
+                    (transfere hospital-cheio :espera :raio-x)))
+        )
+      ))
